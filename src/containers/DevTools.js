e801d046dbdafa4fd21bc4a3bc76cd60cb5238b4
Nicolas Chalet <nicolas.chalet@gmail.com>
2016-03-09 21:40:44 +0100

    DevTools, title

--- src/containers/DevTools.js
+++ src/containers/DevTools.js
@@ -3,12 +3,13 @@ import { createDevTools } from 'redux-devtools';
 import LogMonitor from 'redux-devtools-log-monitor';
 import DockMonitor from 'redux-devtools-dock-monitor';
 
-const showDevTools = true;
+const showDevTools = false;
 export default createDevTools(
   <DockMonitor
     toggleVisibilityKey='ctrl-h'
     changePositionKey='ctrl-q'
     defaultIsVisible={showDevTools}
+    defaultSize={0.4}
   >
     <LogMonitor />
   </DockMonitor>
